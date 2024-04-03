0dceaf49f00907e10c1d430adf8f7fc06b815f44
Deniss Solovjovs <deniss11sol@gmail.com>
2024-04-03 20:20:36 +0300

    Props as a component's API utilised for StarsRating component

--- src/StarRating.js
+++ src/StarRating.js
@@ -1,37 +1,66 @@
 import { useState } from 'react';
+import PropTypes from 'prop-types';
 
-const containerStyle = {
-  display: 'flex',
-  alignItems: 'center',
-  gap: '16px',
-};
-
+// When ossible keep this in the global state, so it is not read agin on every re-render
 const starContainerStyle = {
   display: 'flex',
   gap: '1px',
 };
 
-const textStyle = {
-  lineHeight: '1',
-  margin: '0',
+StarRating.propTypes = {
+  maxRating: PropTypes.number,
+  defaultRating: PropTypes.number,
+  color: PropTypes.string,
+  size: PropTypes.number,
+  messages: PropTypes.array,
+  onSetRating: PropTypes.func,
 };
 
-export default function StarRating({ maxRating }) {
-  const [rating, setRating] = useState(0);
-  const [clicked, setClicked] = useState(false);
+export default function StarRating({
+  maxRating = 5,
+  defaultRating = 0,
+  color = '#fcc419',
+  size = 24,
+  messages = [],
+  onSetRating,
+}) {
+  const [rating, setRating] = useState(defaultRating);
+  const [clicked, setClicked] = useState(defaultRating ? true : false);
+
+  const textStyle = {
+    lineHeight: '1',
+    margin: '0',
+    fontSize: size * 0.9,
+    color,
+  };
+
+  const containerStyle = {
+    display: 'flex',
+    alignItems: 'center',
+    gap: '16px',
+  };
 
   function handleClick(id) {
     if (rating === id && clicked) {
       setClicked(false);
       setRating(0);
+      onSetRating(0);
     } else {
       setClicked(true);
       setRating(id);
+      onSetRating(id);
     }
   }
 
   function onMouseEnter(id) {
-    if (!clicked) setRating(id);
+    if (!clicked) {
+      /* Do not want hover to affect associated states.
+       * Hower affect the stars inside this component.
+       * But only a clicked element is affirmitive
+       */
+      // onSetRating(0);
+      setRating(id);
+    }
   }
 
   function onMouseLeave(id) {
@@ -49,6 +78,8 @@ export default function StarRating({ maxRating }) {
             onClick={handleClick}
             onMouseEnter={onMouseEnter}
             onMouseLeave={onMouseLeave}
+            size={size}
+            color={color}
           />
         ))}
       </div>
@@ -57,20 +88,28 @@ export default function StarRating({ maxRating }) {
   );
 }
 
-const starStyle = {
-  width: '24px',
-  height: '24px',
-  display: 'block',
-  cursor: 'pointer',
-};
+function Star({
+  id,
+  onClick,
+  currentRating,
+  onMouseLeave,
+  onMouseEnter,
+  size: widthHeight,
+  color,
+}) {
+  const starStyle = {
+    width: `${widthHeight}px`,
+    height: `${widthHeight}px`,
+    display: 'block',
+    cursor: 'pointer',
+  };
 
-function Star({ id, onClick, currentRating, onMouseLeave, onMouseEnter }) {
   const fullStar = (
     <svg
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 20 20"
-      fill="#000"
-      stroke="#000"
+      fill={color}
+      stroke={color}
     >
       <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
     </svg>
@@ -81,7 +120,7 @@ function Star({ id, onClick, currentRating, onMouseLeave, onMouseEnter }) {
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
-      stroke="#000"
+      stroke={color}
     >
       <path
         strokeLinecap="round"
